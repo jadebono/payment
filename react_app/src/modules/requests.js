d40ae1c66e643086a05a4da818508cab6ba8c7e8
Joseph Anthony Debono <joe@jadebono.com>
2024-02-06 07:50:30 +0100

    added dotenv to react frontend

--- react_app/src/modules/requests.js
+++ react_app/src/modules/requests.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000"; // Adjust this as necessary
+const BASE_URL = `${process.env.REACT_APP_HOST}${process.env.REACT_APP_SERVER}`; // Adjust this from .env as necessary
 
 // async function to send payment details to backend.
 export async function charge(data) {
@@ -23,11 +23,9 @@ export async function charge(data) {
 
 // async funtion to retrieve user details for login page
 export async function getTrans() {
-  console.log("asking nicely for transaction data");
   const transactions = await axios
     .get(`${BASE_URL}/transactions/retrieve`)
     .then((res) => res.data)
     .catch((err) => false);
-  console.log(`your retrieved transactions are:`, transactions);
   return transactions;
 }
